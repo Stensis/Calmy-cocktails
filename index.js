43e99530e948c6eb1f405081569a50f7ce8f205c
stensis <irenenjuguna98@gmail.com>
2022-06-24 19:07:03 +0300

    added styling

--- index.js
+++ index.js
@@ -1,77 +1,42 @@
 
-function(){
-  var_myContentArea=document.getElementById("myContentArea");
-  var_mySearchButton=document.getElementById("mySearchButton");
-  _mySearchButton.onclick=getData;
-}
-function getData(){
-  let_mySearchField=document.getElementById("mySearchField");
-  $.ajax({
-    url:'https://cocktails1.p.rapidapi.com/popular.php'+_mySearchField.value,
-    method:"GET",
-    dataType:"json",
-    success:function(data){
-      var str=
-    }
-  })
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const input=document.getElementById("#search")
-// .addEventListener("click",button)
-// function button(){
-//     alert('online')
-// }
-
-
-
-
-
-
-// const button=document.getElementsByClassName("primary-btn").addEventlistener("onclick",button)
-// function button(){
-//     alert(welcome)
-// }
-
-
-
-// const icon = document.getElementsByClassName('search-box');
-// const input = document.queryselector('.text')
-
-// menu.onclick =() =>{
-//     menu.classList
-// }
+// get menuBtn
+let menuBtn = document.getElementById("menuBtn");
+// get id for menuSlot div
+let menuData = document.getElementById("menuData");
 //  API KEY
-
-// const axios = require("axios");
-
-// const options = {
-//   method: 'GET',
-//   url: 'https://cocktails1.p.rapidapi.com/popular.php',
-//   headers: {
-//     'X-RapidAPI-Key': 'f65e2f85c4mshfd238deadc30f07p192954jsnd2b2e354f582',
-//     'X-RapidAPI-Host': 'cocktails1.p.rapidapi.com'
-//   }
-// };
-
-// axios.request(options).then(function (response) {
-// 	console.log(response.data);
-// }).catch(function (error) {
-// 	console.error(error);
-// });
+const options = {
+    method: 'GET',
+    url: 'https://the-cocktail-db.p.rapidapi.com/popular.php',
+    headers: {
+        'X-RapidAPI-Key': 'f65e2f85c4mshfd238deadc30f07p192954jsnd2b2e354f582',
+        'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com'
+    }
+};
+// axios requesting data.
+axios.request(options).then(function (response) {
+    console.log(response.data);
+    let data = response.data;
+    // adding event listener
+    menuBtn.addEventListener('click',function(){
+        menuData.innerHTML = `<center><h1><span><u>Menu List</u></span></h1></center>`;
+        data.drinks.map((drink)=>{
+            menuData.innerHTML += `
+            <div class="col">
+                <div class="card">
+                    <img src="${drink.strDrinkThumb}" alt="Avatar" style="width:100%">
+                    <div class="container">
+                        <h4><b>${drink.strDrink}</b></h4>
+                        <p>${drink.strCategory}</p>
+                    </div>
+                </div>
+            </div>
+            `;
+        });
+    },false);
+//  handles the error.
+}).catch(function (error) {
+    console.log(error);
+    menuBtn.addEventListener('click', ()=>{
+        menuData.innerHTML = `<center style="padding-bottom:30px"><h2><span>Could not Access Data</span></h2></center>`;
+    },false);
+});
