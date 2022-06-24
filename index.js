2d9539bf3c2c179b48a18470bb41b47d345bd944
stensis <irenenjuguna98@gmail.com>
2022-06-24 18:47:03 +0300

    index.html

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 
+function(){
+  var_myContentArea=document.getElementById("myContentArea");
+  var_mySearchButton=document.getElementById("mySearchButton");
+  _mySearchButton.onclick=getData;
+}
+function getData(){
+  let_mySearchField=document.getElementById("mySearchField");
+  $.ajax({
+    url:'https://cocktails1.p.rapidapi.com/popular.php'+_mySearchField.value,
+    method:"GET",
+    dataType:"json",
+    success:function(data){
+      var str=
+    }
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const input=document.getElementById("#search")
 // .addEventListener("click",button)
 // function button(){
@@ -25,19 +59,19 @@
 // }
 //  API KEY
 
-const axios = require("axios");
+// const axios = require("axios");
 
-const options = {
-  method: 'GET',
-  url: 'https://cocktails1.p.rapidapi.com/popular.php',
-  headers: {
-    'X-RapidAPI-Key': 'f65e2f85c4mshfd238deadc30f07p192954jsnd2b2e354f582',
-    'X-RapidAPI-Host': 'cocktails1.p.rapidapi.com'
-  }
-};
+// const options = {
+//   method: 'GET',
+//   url: 'https://cocktails1.p.rapidapi.com/popular.php',
+//   headers: {
+//     'X-RapidAPI-Key': 'f65e2f85c4mshfd238deadc30f07p192954jsnd2b2e354f582',
+//     'X-RapidAPI-Host': 'cocktails1.p.rapidapi.com'
+//   }
+// };
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
