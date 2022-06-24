8d23fa723b5879840f4de7c28161fb32c82919e2
stensis <irenenjuguna98@gmail.com>
2022-06-24 22:43:10 +0300

    requesting the data

--- index.js
+++ index.js
@@ -13,7 +13,29 @@ const options = {
     }
 };
 // axios requesting data.
-
+axios.request(options).then(function (response) {
+    console.log(response.data);
+    let data = response.data;
+    // adding event listener
+    menuBtn.addEventListener('click',function(){
+        menuData.innerHTML = `<center><h1><span><u>Menu List</u></span></h1></center>`;
+        data.drinks.map((drink)=>{
+            menuData.innerHTML += `
+            <div class="col">
+                <div class="card">
+                    <img src="${drink.strDrinkThumb}" alt="Avatar" style="width:100%">
+                    <div class="container">
+                        <h4><b>${drink.strDrink}</b></h4>
+                        <p>${drink.strCategory}</p>
+                    </div>
+                </div>
+            </div>
+            `;
+        });
+    },false);
+//  handles the error.
+}).catch(function (error) {
+    console.log(error);
     menuBtn.addEventListener('click', ()=>{
         menuData.innerHTML = `<center style="padding-bottom:30px"><h2><span>Could not Access Data</span></h2></center>`;
     },false);
